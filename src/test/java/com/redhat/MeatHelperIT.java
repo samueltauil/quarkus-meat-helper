0cdc9583a0f510ae5a996efa1ec331cdef49c073
Samuel Tauil <samueltauil@gmail.com>
2023-05-09 09:13:36 -0400

    idempotent resources

--- src/test/java/com/redhat/MeatHelperIT.java
+++
@@ -1,8 +0,0 @@
-package com.redhat;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class MeatHelperIT extends MeatHelperTest {
-    // Execute the same tests but in packaged mode.
-}
